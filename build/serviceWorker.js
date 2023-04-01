a53afd76c030a2db53b37f7b52fb7c75e2451920
Parathesis <129522916+Parathesis@users.noreply.github.com>
2023-04-01 14:06:50 +0530

    Add files via upload

---
+++ build/serviceWorker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "offline-page-cache";
+const OFFLINE_PAGE_URL = "/offline.html";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([OFFLINE_PAGE_URL]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match(OFFLINE_PAGE_URL);
+      });
+    })
+  );
+});
